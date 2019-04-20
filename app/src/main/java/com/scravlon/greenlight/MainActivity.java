0b1e1a25e13cb9580808e7efd1735f4b3cef47b4
Scravlon <haokokyonng10@gmail.com>
2019-04-19 21:56:06 -0400

    Program structure updated

--- app/src/main/java/com/scravlon/greenlight/MainActivity.java
+++ app/src/main/java/com/scravlon/greenlight/MainActivity.java
@@ -1,9 +1,13 @@
 package com.scravlon.greenlight;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -14,6 +18,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int MY_PERMISSIONS_REQUEST_COARSE_LOCATION = 99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         final EditText editnumber = findViewById(R.id.edit_number);
         final EditText editpass = findViewById(R.id.edit_password);
         Button butLogin = findViewById(R.id.but_login);
-
+        requestLocationService();
         butLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,6 +49,47 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    /**
+     * Request the location service from the user, required Android 6.0 and higher
+     *
+     * Reference: https://developer.android.com/training/permissions/requesting
+     */
+    private void requestLocationService() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_COARSE_LOCATION);
+            }
+        } else {
+        }
+
+    }
+
+    /**
+     * Handle the Location request
+     * @param requestCode: Code of request
+     * @param permissions: permission
+     * @param grantResults: result
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_COARSE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                }
+                return;
+            }
+        }
+    }
+
 
     /**
      * Update the activity to the sub Main activity
--- app/src/main/java/com/scravlon/greenlight/subMainActivity.java
+++ app/src/main/java/com/scravlon/greenlight/subMainActivity.java
@@ -1,39 +1,72 @@
 package com.scravlon.greenlight;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-public class subMainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 
+public class subMainActivity extends AppCompatActivity implements homeFragment.OnFragmentInteractionListener, qrFragment.OnFragmentInteractionListener, locationFragment.OnFragmentInteractionListener
+, educationFragment.OnFragmentInteractionListener, moneyFragment.OnFragmentInteractionListener {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
+        Fragment fragment = null;
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    return true;
-                case R.id.navigation_dashboard:
-                    return true;
-                case R.id.navigation_notifications:
-                    return true;
+                    fragment = new homeFragment();
+                    break;
+                case R.id.navigation_money:
+                    fragment = new moneyFragment();
+                    break;
+                case R.id.navigation_qr:
+                    fragment = new qrFragment();
+                    break;
+                case R.id.navigation_location:
+                    fragment = new locationFragment();
+                    break;
+                case R.id.navigation_education:
+                    fragment = new educationFragment();
+                    break;
+
+
             }
-            return false;
+
+            return loadFragment(fragment);
         }
     };
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_main);
 
+        loadFragment(new homeFragment());
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
